c67a82ac444290e23a9176493d499249e1c00ba2
Jelson Stoelben Rodrigues <jelsonrodrigues25@gmail.com>
2023-08-11 09:39:17 -0300

    Stating

--- src/client.rs
+++ src/client.rs
@@ -23,3 +23,93 @@ fn main() {
 
   println!("Closing!!!");
 }
+
+
+/*
+
+use std::{net::{SocketAddrV4, TcpStream}, io::{Write, Read}, process::exit};
+
+pub mod commom;
+use commom::*;
+
+fn main() {
+    println!("This is the Client!!!");
+
+    let connection = std::net::TcpStream::connect(SocketAddrV4::new(ADDRESS, PORT));
+
+    let mut stream: std::net::TcpStream;
+    match connection {
+        Ok(ok) => stream = ok,
+        Err(error) => {
+            println!("ERROR occured!! Kind: {}", error.kind());
+            println!("{error}");
+            exit(-1);
+        },
+    }
+
+    let (key, iv) = handle_security_protocol(&mut stream); 
+
+    let aes_key_enc = openssl::aes::AesKey::new_encrypt(&key).unwrap();
+    let aes_key_dec = openssl::aes::AesKey::new_decrypt(&key).unwrap();
+
+    let mut iv_enc = iv.clone();
+    let mut iv_dec = iv.clone();
+
+    let mut uuid_bytes:Vec<u8> = Vec::new();
+    decrypt_receiving_data(&mut stream, &mut uuid_bytes, &aes_key_dec, &mut iv_dec);
+    
+    let mut uuid_bytes_static = [0_u8; 16];
+    uuid_bytes_static.copy_from_slice(&uuid_bytes[0..16]);
+
+    let uuid = uuid::Uuid::from_bytes_le(uuid_bytes_static);
+    println!("UUID got {uuid}");
+
+    for _ in 0..NUMBERS_TO_SEND {
+        send_random_number(&mut stream, &aes_key_enc, &mut iv_enc);
+    }
+
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut number_bytes = [0_u8; 8];
+    for _ in 0..NUMBERS_TO_SEND {
+        decrypt_receiving_data(&mut stream, &mut buffer, &aes_key_dec, &mut iv_dec);
+        number_bytes.copy_from_slice(&buffer[..8]);
+        let number = i64::from_le_bytes(number_bytes);
+        println!("Number {number}");
+    }
+}
+
+fn send_random_number(stream: &mut std::net::TcpStream, aes_key_enc: &openssl::aes::AesKey, iv_enc: &mut [u8]) {
+    let number_to_send:i64 = rand::random::<i64>() % 2048;
+    encrypt_send_data(stream, &number_to_send.to_le_bytes(), &aes_key_enc, iv_enc);
+    println!("Number sent {number_to_send}");
+}
+
+fn handle_security_protocol(stream : &mut TcpStream) -> (Vec<u8>, Vec<u8>){
+    // Get the public RSA key
+    let mut bytes_size = [0_u8; 8];
+    stream.read_exact(&mut bytes_size).unwrap();
+    let size_to_read = usize::from_le_bytes(bytes_size);
+    let mut rsa_public_bytes_der = get_initialized_buffer(size_to_read);
+    stream.read_exact(&mut rsa_public_bytes_der).unwrap();
+
+    // Generate the AES key
+    let mut key = get_initialized_buffer(AES_KEY_SIZE / 8);
+    let mut iv = get_initialized_buffer(AES_KEY_SIZE / 4);
+    openssl::rand::rand_bytes(&mut key).unwrap();
+    openssl::rand::rand_bytes(&mut iv).unwrap();
+
+    // Send back
+    let rsa_key = openssl::rsa::Rsa::public_key_from_der(&rsa_public_bytes_der).unwrap();
+    
+    let mut key_enc = get_initialized_buffer(rsa_key.size() as usize);
+    rsa_key.public_encrypt(&key, &mut key_enc, openssl::rsa::Padding::PKCS1).unwrap();
+
+    let mut iv_enc = get_initialized_buffer(rsa_key.size() as usize);
+    rsa_key.public_encrypt(&iv, &mut iv_enc, openssl::rsa::Padding::PKCS1).unwrap();
+    
+    stream.write_all(&key_enc).unwrap();
+    stream.write_all(&iv_enc).unwrap();
+
+    (key, iv)
+}
+*/
--- src/server.rs
+++ src/server.rs
@@ -39,3 +39,115 @@ fn main() {
 
   }
 }
+
+/*
+
+// Generate a random uuid
+// encrypt the ID
+// Send it to the client
+
+use std::{net::{SocketAddrV4, TcpStream}, io::{Write, Read}};
+use openssl::aes::AesKey;
+
+pub mod commom;
+use commom::*;
+
+fn main() {
+    println!("This is the Server side");
+
+    // Generate uuid
+    let server_uuid = generate_uuid();
+    println!("server - UUID: {server_uuid}");
+
+    // Create listener
+    let listener = std::net::TcpListener::bind(SocketAddrV4::new(ADDRESS, PORT))
+        .expect("Unable to create listener!!");
+    println!("IP: {}", listener.local_addr().unwrap());
+
+    loop {
+        for item in listener.incoming() {
+            if let Ok(stream) = item {
+                handle_connection(stream);
+            }
+        }
+    }
+}
+
+fn generate_uuid() -> uuid::Uuid {
+    return uuid::Uuid::new_v4();
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let addr = stream.peer_addr().expect("Error");
+    println!("Address incoming {addr}");
+
+    let (aes_key, iv) = handle_security_protocol(&mut stream);
+
+    let aes_key_enc = openssl::aes::AesKey::new_encrypt(&aes_key).unwrap();
+    let aes_key_dec = openssl::aes::AesKey::new_decrypt(&aes_key).unwrap();
+
+    let mut iv_enc = iv.clone();
+    let mut iv_dec = iv.clone();
+
+    handle_send_random_uuid_request(&mut stream, &aes_key_enc, &mut iv_enc);
+
+    // Get the Number of Request and display
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut number_bytes = [0_u8; 8];
+    for _ in 0..NUMBERS_TO_SEND {
+        decrypt_receiving_data(&mut stream, &mut buffer, &aes_key_dec, &mut iv_dec);
+        number_bytes.copy_from_slice(&buffer[..8]);
+        let number = i64::from_le_bytes(number_bytes);
+        println!("Number {number}");
+    }
+
+    for _ in 0..NUMBERS_TO_SEND {
+        send_random_number(&mut stream, &aes_key_enc, &mut iv_enc);
+    }
+    
+}
+
+fn send_random_number(stream: &mut std::net::TcpStream, aes_key_enc: &openssl::aes::AesKey, iv_enc: &mut [u8]) {
+    let number_to_send:i64 = rand::random::<i64>() % 2048 + 10000;
+    encrypt_send_data(stream, &number_to_send.to_le_bytes(), &aes_key_enc, iv_enc);
+    println!("Number sent {number_to_send}");
+}
+
+fn handle_send_random_uuid_request(stream: &mut TcpStream, aes_enc_key: &AesKey, iv_enc: &mut [u8]) {
+    let random_uuid = generate_uuid();
+    println!("UUID sent {random_uuid}");
+    encrypt_send_data(stream, &random_uuid.to_bytes_le(), aes_enc_key, iv_enc);
+}
+
+fn handle_security_protocol(stream: &mut TcpStream) -> (Vec<u8>, Vec<u8>) {
+    // Generate asymetric RSA key
+    let rsa_key = openssl::rsa::Rsa::generate(RSA_KEY_SIZE).unwrap();
+
+    // Format as DER
+    let der_format = rsa_key.public_key_to_der().unwrap();
+
+    // Send to the client
+    stream.write(&der_format.len().to_le_bytes()).unwrap();  // Writes usize bytes and send to client the ammount of data to retrieve from the stream
+    stream.write(&der_format).unwrap();
+
+    // Get the bytes of AES Key
+    let mut aes_key_bytes_enc: Vec<u8> = get_initialized_buffer(rsa_key.size() as usize);
+    stream.read_exact(&mut aes_key_bytes_enc).unwrap();
+
+    // Get the bytes of IV for AES Key
+    let mut iv_bytes_enc = get_initialized_buffer(rsa_key.size() as usize);
+    stream.read_exact(&mut iv_bytes_enc).unwrap();
+
+    let mut aes_key_bytes: Vec<u8> = get_initialized_buffer(rsa_key.size() as usize);
+    let mut iv_bytes: Vec<u8> = get_initialized_buffer(rsa_key.size() as usize);
+
+    let size_key = rsa_key.private_decrypt(&aes_key_bytes_enc, &mut aes_key_bytes, openssl::rsa::Padding::PKCS1).unwrap();
+    let size_iv = rsa_key.private_decrypt(&iv_bytes_enc, &mut iv_bytes, openssl::rsa::Padding::PKCS1).unwrap();
+
+    aes_key_bytes.truncate(size_key);
+    iv_bytes.truncate(size_iv);
+
+    (aes_key_bytes, iv_bytes)
+}
+
+*/
