88a434eaf0fea00aa8c94a0dff54e146c321c85c
Jelson Stoelben Rodrigues <jelsonrodrigues25@gmail.com>
2023-08-14 11:20:16 -0300

    Remove client ports

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,9 @@
 // pub const ADDRESS:std::net::Ipv4Addr = std::net::Ipv4Addr::LOCALHOST;
 pub const ADDRESS:std::net::Ipv4Addr = std::net::Ipv4Addr::new(0, 0, 0, 0);
-pub const PORT_CLIENT:u16 = 5823;
+// pub const PORT_CLIENT:u16 = 5823;
 pub const PORT_SERVER:u16 = 5824;
 pub const BUFFER_SIZE:usize = 1 << 11;
-pub const CLIENT_ADDR_SOCKET: std::net::SocketAddrV4 = std::net::SocketAddrV4::new(ADDRESS, PORT_CLIENT);
+// pub const CLIENT_ADDR_SOCKET: std::net::SocketAddrV4 = std::net::SocketAddrV4::new(ADDRESS, PORT_CLIENT);
 pub const SERVER_ADDR_SOCKET: std::net::SocketAddrV4 = std::net::SocketAddrV4::new(ADDRESS, PORT_SERVER);
 pub const RSA_KEY_SIZE : u32 = 1024;
 
